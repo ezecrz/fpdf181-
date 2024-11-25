739144310d7ad6c60ade32ffa8f50e42b36cff9e
ezecrz <otro.ezequielcruz@gmail.com>
2024-11-24 18:42:25 -0600

    Update fpdf.php

--- fpdf.php
+++ fpdf.php
@@ -102,115 +102,80 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 	$this->WithAlpha = false;
 	$this->ws = 0;
 	// Font path
-	// Configurar el path de las fuentes
-    if (defined('FPDF_FONTPATH')) {
-        $this->fontpath = FPDF_FONTPATH;
-        $this->ensureTrailingSlash();
-    } elseif (is_dir(dirname(__FILE__) . '/font')) {
-        $this->fontpath = dirname(__FILE__) . '/font/';
-    } else {
-        $this->fontpath = '';
-    }
+	if(defined('FPDF_FONTPATH'))
+	{
+		$this->fontpath = FPDF_FONTPATH;
+		if(substr($this->fontpath,-1)!='/' && substr($this->fontpath,-1)!='\\')
+			$this->fontpath .= '/';
+	}
+	elseif(is_dir(dirname(__FILE__).'/font'))
+		$this->fontpath = dirname(__FILE__).'/font/';
+	else
+		$this->fontpath = '';
+	// Core fonts
+	$this->CoreFonts = array('courier', 'helvetica', 'times', 'symbol', 'zapfdingbats');
+	// Scale factor
+	if($unit=='pt')
+		$this->k = 1;
+	elseif($unit=='mm')
+		$this->k = 72/25.4;
+	elseif($unit=='cm')
+		$this->k = 72/2.54;
+	elseif($unit=='in')
+		$this->k = 72;
+	else
+		$this->Error('Incorrect unit: '.$unit);
+	// Page sizes
+	$this->StdPageSizes = array('a3'=>array(841.89,1190.55), 'a4'=>array(595.28,841.89), 'a5'=>array(420.94,595.28),
+		'letter'=>array(612,792), 'legal'=>array(612,1008));
+	$size = $this->_getpagesize($size);
+	$this->DefPageSize = $size;
+	$this->CurPageSize = $size;
+	// Page orientation
+	$orientation = strtolower($orientation);
+	if($orientation=='p' || $orientation=='portrait')
+	{
+		$this->DefOrientation = 'P';
+		$this->w = $size[0];
+		$this->h = $size[1];
+	}
+	elseif($orientation=='l' || $orientation=='landscape')
+	{
+		$this->DefOrientation = 'L';
+		$this->w = $size[1];
+		$this->h = $size[0];
+	}
+	else
+		$this->Error('Incorrect orientation: '.$orientation);
+	$this->CurOrientation = $this->DefOrientation;
+	$this->wPt = $this->w*$this->k;
+	$this->hPt = $this->h*$this->k;
+	// Page rotation
+	$this->CurRotation = 0;
+	// Page margins (1 cm)
+	$margin = 28.35/$this->k;
+	$this->SetMargins($margin,$margin);
+	// Interior cell margin (1 mm)
+	$this->cMargin = $margin/10;
+	// Line width (0.2 mm)
+	$this->LineWidth = .567/$this->k;
+	// Automatic page break
+	$this->SetAutoPageBreak(true,2*$margin);
+	// Default display mode
+	$this->SetDisplayMode('default');
+	// Enable compression
+	$this->SetCompression(true);
+	// Set default PDF version number
+	$this->PDFVersion = '1.3';
 }
-
-// Asegura que la ruta de la fuente termine con una barra (/) o barra invertida (\)
-private function ensureTrailingSlash()
-{
-    if (substr($this->fontpath, -1) != '/' && substr($this->fontpath, -1) != '\\') {
-        $this->fontpath .= '/';
-    }
-}
-
-// Configurar el factor de escala
-private function setScaleFactor($unit)
-{
-    switch ($unit) {
-        case 'pt':
-            $this->k = 1;
-            break;
-        case 'mm':
-            $this->k = 72 / 25.4;
-            break;
-        case 'cm':
-            $this->k = 72 / 2.54;
-            break;
-        case 'in':
-            $this->k = 72;
-            break;
-        default:
-            $this->Error('Incorrect unit: ' . $unit);
-    }
-}
-
-// Configurar los tamaños de página
-private function setPageSize($size)
-{
-    $this->StdPageSizes = array(
-        'a3' => array(841.89, 1190.55),
-        'a4' => array(595.28, 841.89),
-        'a5' => array(420.94, 595.28),
-        'letter' => array(612, 792),
-        'legal' => array(612, 1008)
-    );
-    return $this->_getpagesize($size);
-}
-
-// Configurar la orientación de la página
-private function setOrientation($orientation, $size)
-{
-    $orientation = strtolower($orientation);
-    if ($orientation == 'p' || $orientation == 'portrait') {
-        $this->DefOrientation = 'P';
-        $this->w = $size[0];
-        $this->h = $size[1];
-    } elseif ($orientation == 'l' || $orientation == 'landscape') {
-        $this->DefOrientation = 'L';
-        $this->w = $size[1];
-        $this->h = $size[0];
-    } else {
-        $this->Error('Incorrect orientation: ' . $orientation);
-    }
-    $this->CurOrientation = $this->DefOrientation;
-}
-
-// Configurar los márgenes y otras propiedades de la página
-private function setPageProperties($margin)
-{
-    $this->SetMargins($margin, $margin);
-    $this->cMargin = $margin / 10;
-    $this->LineWidth = .567 / $this->k;
-    $this->SetAutoPageBreak(true, 2 * $margin);
-    $this->SetDisplayMode('default');
-    $this->SetCompression(true);
-    $this->PDFVersion = '1.3';
-}
-
-// Función principal que llama a todas las configuraciones
-public function configurePDF($unit, $size, $orientation)
-{
-    $this->setFontPath();
-    $this->setScaleFactor($unit);
-    
-    $size = $this->setPageSize($size);
-    $this->DefPageSize = $size;
-    $this->CurPageSize = $size;
-    
-    $this->setOrientation($orientation, $size);
-    
-    // Márgenes por defecto (1 cm)
-    $margin = 28.35 / $this->k;
-    $this->setPageProperties($margin);
-}
-
 
 function SetMargins($left, $top, $right=null)
 {
 	// Set left, top and right margins
 	$this->lMargin = $left;
 	$this->tMargin = $top;
-	if($right===null){
+	if($right===null)
 		$right = $left;
-	}
 	$this->rMargin = $right;
 }
 
